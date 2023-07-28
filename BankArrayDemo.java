89fed1920b73deab320f3f4430aa600cf071ea17
ROBERT <mawulegabriel@gmail.com>
2023-07-28 04:14:54 +0000

    array simulation of the bank

---
+++ BankArrayDemo.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+
+public class BankArrayDemo {
+    public static void main(String[] args){
+        ArrayList<BankAccount> list = new ArrayList<BankAccount>();
+
+        list.add(new BankAccount(100));
+        list.add(new BankAccount(1000));
+        list.add(new BankAccount(800));
+
+        for(int index=0; index<list.size(); index++){
+            System.out
+        }
+    }
+
+}
