9f60d0d6093ca8a505a52660d1c6d0fb136f1bf2
ROBERT <mawulegabriel@gmail.com>
2023-07-29 15:01:53 +0000

    Metric demo

---
+++ Metrics.java
@@ -0,0 +1,11 @@
+public class Metrics {
+
+    public static double milesToKilometers(double m) {
+        return m * 1.609;
+    }
+
+    public static double kilometersToMiles(double k) {
+        return k / 1.609;
+    }
+
+}
